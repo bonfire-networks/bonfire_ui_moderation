ed4c7ab790b0793ef5f48efd636fac9a6016fa16
Mayel de Borniol <git@mayel.space>
2024-04-03 14:06:13 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/components/list/flags_live.ex
+++ lib/components/list/flags_live.ex
@@ -1,5 +1,13 @@
 defmodule Bonfire.UI.Moderation.FlagsLive do
   use Bonfire.UI.Common.Web, :stateful_component
+
+  declare_extension("UI for moderation",
+    icon: "bxs:flag-alt",
+    emoji: "🚩",
+    description:
+      l("User interfaces for flagging content or users, and reviewing and acting on them.")
+  )
+
   #
   prop feed_count, :string, default: ""
 
