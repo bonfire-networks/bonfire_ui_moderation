f25b3cf917d2b67a7e0329751400baf268d489a2
ivan <ivanminutillo@outlook.it>
2024-07-12 22:11:00 +0200

    warnings

--- lib/runtime_config.ex
+++ lib/runtime_config.ex
@@ -8,7 +8,7 @@ defmodule Bonfire.UI.Moderation.RuntimeConfig do
   NOTE: you can override this default config in your app's `runtime.exs`, by placing similarly-named config keys below the `Bonfire.Common.Config.LoadExtensionsConfig.load_configs()` line
   """
   def config do
-    import Config
+    # import Config
 
     # config :bonfire_ui_social,
     #   modularity: :disabled
