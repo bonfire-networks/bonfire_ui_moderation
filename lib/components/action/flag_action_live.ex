68537cc1edd468ef0d7392b500146e87fb141f52
Mayel de Borniol <git@mayel.space>
2024-09-01 11:11:14 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1018

--- lib/components/action/flag_action_live.ex
+++ lib/components/action/flag_action_live.ex
@@ -4,6 +4,7 @@ defmodule Bonfire.UI.Moderation.FlagActionLive do
 
   prop object, :any
   prop parent_id, :string, default: nil
+  prop permalink, :string, default: nil
   prop label, :string, default: nil
   prop flagged, :any, default: nil
   prop hide_icon, :boolean, default: false
