85414280b192d579d4cec046dae048cb9fa3e0dc
ivan <ivanminutillo@outlook.it>
2025-04-09 16:44:13 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/1177

--- test/views/flags_notifications_test.exs
+++ test/views/flags_notifications_test.exs
@@ -30,6 +30,28 @@ defmodule Bonfire.UI.Moderation.Notifications.Flag.Test do
       |> assert_has("[data-id=feed] article", text: "flagged")
     end
 
+    test "flags on a post (which admin has permission to see) in admin's notifications has subject of the object flagged preloaded" do
+      some_account = fake_account!()
+      {:ok, someone} = Users.make_admin(fake_user!(some_account))
+      poster = fake_user!()
+
+      # Create a post
+      attrs = %{post_content: %{html_body: "epic html post"}}
+
+      assert {:ok, post} =
+               Posts.publish(current_user: poster, post_attrs: attrs, boundary: "public")
+
+      # Flag the post
+      flagger = fake_user!()
+      Flags.flag(flagger, post)
+
+      # Check notifications
+      conn(user: someone, account: some_account)
+      |> visit("/notifications")
+      |> assert_has("[data-id=feed] article", text: poster.profile.name)
+      |> assert_has("[data-id=feed] article", text: "flagged")
+    end
+
     test "flags on a post (which admin does not explicitly have permission to see) in admin's notifications" do
       alice_account = fake_account!()
       alice = fake_admin!(alice_account)
